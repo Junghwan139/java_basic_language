b4ba94d397de081a246d8aadcde948cab8de7959
Junghwan <balance139@gmail.com>
2023-02-04 20:49:27 +0900

    message3

--- Test2.java
+++ Test2.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class Test2 {
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("abc");
+        System.out.println("abc");
+        System.out.println("abc");
+        System.out.println("abc");
+    }
 }

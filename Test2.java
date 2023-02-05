1aff72b3ae15b8cb895a3421be71f441875a54b7
Junghwan <balance139@gmail.com>
2023-02-05 10:25:29 +0900

    message6

--- Test2.java
+++
@@ -1,9 +0,0 @@
-public class Test2 {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("abc");
-        System.out.println("abc");
-        System.out.println("abc");
-        System.out.println("abc");
-    }
-}

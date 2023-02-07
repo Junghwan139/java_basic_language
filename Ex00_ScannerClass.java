e9191dbc054e2c9467cf80ec287df2b0169dd6f7
Junghwan <balance139@gmail.com>
2023-02-07 12:26:43 +0900

    test from the company computer

---
+++ Ex00_ScannerClass.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Ex00_ScannerClass {
+    public static void main(String[] args) {
+        System.out.println(12000  );
+        System.out.print("이름을 입력하세요 : ");
+        //입력 받을 때 하위 2줄 copy
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.print("당신의 이름은 "+name+"입니다");
+
+    }
+}

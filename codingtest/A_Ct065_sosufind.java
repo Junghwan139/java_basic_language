e013aaa289f2878a538c2297db280bfff55d0b77
Junghwan <balance139@gmail.com>
2023-03-07 07:35:56 +0900

    23.03.07 morning study

--- codingtest/A_Ct065_sosufind.java
+++ codingtest/A_Ct065_sosufind.java
@@ -1,5 +1,8 @@
 package codingtest;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class A_Ct065_sosufind {
 
     public static void main(String[] args) {
@@ -22,11 +25,17 @@ public class A_Ct065_sosufind {
         int n = 10;
         int answer = 0;
 
+
+/*
+
         int count = 0;
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
                 if (i % j == 0) {
                     count++;
+                    if(count>=3){
+                        break;
+                    }
                 }
 
             }
@@ -37,7 +46,34 @@ public class A_Ct065_sosufind {
 
         }
 
+*/
+
+        Map<Integer,Integer> mp = new HashMap<>();
+
+        for(int i = 2; i<=n;i++){
+
+            mp.put(i,0);
+        }
+        System.out.println(mp);
+
+        int count = 0;
+        for(int a : mp.keySet()){
+           for(int i = 1;i<=a;i++){
+               if(a%i==0){
+                   count++;
+               }
+               if(count==2){
+                   mp.put(a,1);
+               }
+           }
+        }
+
+        System.out.println(mp);
+
+
+
         System.out.println(answer);
+
 
 
     }

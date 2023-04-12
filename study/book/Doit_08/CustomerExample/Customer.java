8180ec74033481b949d1ed8434565af3f9c5390c
Junghwan Lim <balance139@gmail.com>
2023-04-13 07:18:06 +0900

    23.04.13

--- study/book/Doit_08/CustomerExample/Customer.java
+++ study/book/Doit_08/CustomerExample/Customer.java
@@ -3,15 +3,29 @@ package study.book.Doit_08.CustomerExample;
 // 1-2)
 class Customer {
 
-    private int customerID;
-    private String customerName;
-    private String customerGrade;
+    protected int customerID;
+    protected String customerName;
+    protected String customerGrade;
     int bonusPoint;
     double bonusRatio;
+
+    /*
 
     public Customer() {
         customerGrade = "SILVER";
         bonusRatio = 0.01;
+        System.out.println("Customer() 생성자 호출"); // 상위 클래스 생성할 때 콘솔 출력문
+        // Customer인스턴스가 생성되면 이 호출문이 출력될 것임.
+
+    }
+
+*/
+
+    public Customer(int customerID, String customerName){
+        this.customerID = customerID;
+        this.customerName = customerName;
+        customerGrade = "SILVER";
+        System.out.println("Customer(int,String)생성자 호출");
     }
 
     public int calPrice(int price){
@@ -25,4 +39,27 @@ class Customer {
                 bonusPoint + "입니다.";
     }
 
+    public int getCustomerID() {
+        return customerID;
+    }
+
+    public void setCustomerID(int customerID) {
+        this.customerID = customerID;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerGrade() {
+        return customerGrade;
+    }
+
+    public void setCustomerGrade(String customerGrade) {
+        this.customerGrade = customerGrade;
+    }
 }
--- study/book/Doit_08/CustomerExample/VIPCustomer.java
+++ study/book/Doit_08/CustomerExample/VIPCustomer.java
@@ -4,14 +4,32 @@ class VIPCustomer extends Customer {
     private int agentID;
     double saleRatio;
 
-    public VIPCustomer() {
+    /*
 
+    public VIPCustomer() {
+        customerGrade = "VIP";
         bonusRatio = 0.05;
         saleRatio = 0.1;
-        System.out.println("VIPCustomer() 생성자 호출");
+        System.out.println("VIPCustomer() 생성자 호생");
+
     }
 
-    public int getAgentID() {
+    */
+
+    public VIPCustomer(int customerID, String customerName, int agentID){
+        super(customerID,customerName);
+        customerGrade = "VIP";
+        bonusRatio = 0.05;
+        saleRatio = 0.01;
+        this.agentID = agentID;
+        System.out.println("VIPCustomer(int, String) 생성자 호출");
+
+
+    }
+
+
+    public int getAgentID()
+    {
         return agentID;
     }
 

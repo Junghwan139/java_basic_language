e5f63abf30286c9a67c4e87b85be293168a28005
Junghwan Lim <balance139@gmail.com>
2023-04-17 07:24:38 +0900

    23.04.17

---
+++ classforBackEnd/HtmlCSSJavaScript/Html.java
@@ -0,0 +1,25 @@
+package classforBackEnd.HtmlCSSJavaScript;
+
+public class Html {
+
+    public static void main(String[] args) {
+
+/*
+1. HTML과 첫 만남
+2. HTML 구조 파악하기
+   1) <!DOCTYPE html> : 현재 문서가 HTML5언어로 작성한 웹문서라는 뜻입니다.
+   2) <html> ~ </html> : 웹 문서의 시작과 끝을 나타내는 태그
+   3) <head> ~ </head> : 웹브라우저가 웹문서를 해석하는데 필요한 정보를 입력하는 부분
+   4) <body> ~ </body> : 실제로 웹 브라우저 화면에 나타나는 내용
+
+
+
+
+*/
+
+
+
+
+    }
+
+}

5fb4981190dc09805e5478bdd71931181c4f683e
Junghwan Lim <balance139@gmail.com>
2023-05-20 21:07:54 +0900

    23.05.20

--- Study/A_Java_A_Temp/test.java
+++ Study/A_Java_A_Temp/test.java
@@ -1,4 +1,23 @@
 package Study.A_Java_A_Temp;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class test {
+
+    public static void main(String[] args) {
+
+        List<String> lst = new ArrayList<>();
+
+        lst.add("a");
+        lst.add("a");
+        lst.add("a");
+
+        System.out.println(lst);
+
+
+
+
+    }
+
 }
--- Study/A_Java_B_Book/Doit_09_abstract.java
+++ Study/A_Java_B_Book/Doit_09_abstract.java
@@ -33,11 +33,6 @@ public class Doit_09_abstract {
 
 
 
-
-
-
-
-
     }
 
 }
